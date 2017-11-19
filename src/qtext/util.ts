2be791adffaae36221f1595b637caebfdf4889f3
陈欢 <jingfeng_quietwind@163.com>
2017-11-19 13:18:16 +0800

    fix url bug

--- src/qtext/util.ts
+++ src/qtext/util.ts
@@ -4,7 +4,7 @@ export const isMobile = () => {
 
 export function getYoutubeVideoId(str: string) {
   var videoid = str.match(
-    /(?:https?:\/{2})?(?:w{3}\.)?youtu(?:be)?\.(?:com|be)(?:\/watch\?v=|\/)(?:embed\/([^\s"&]+))/
+    /(?:https?:\/{2})?(?:w{3}\.)?youtu(?:be)?\.(?:com|be)(?:\/watch\?v=|\/)(?:(?:embed\/)?([^\s"&]+))/
   );
   if (videoid != null) {
     return videoid[1];
