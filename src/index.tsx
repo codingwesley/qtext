dd2dd9e500a43c5960b3d627fc262993d6ab201b
陈欢 <jingfeng_quietwind@163.com>
2017-12-25 19:27:46 +0800

    fix code error

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,6 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 import * as classnames from "classnames";
-import { is } from "immutable";
 import { fontCssUrl } from "./config";
 import { decorator } from "./decorator";
 import { ToolBar } from "./ToolBar";
